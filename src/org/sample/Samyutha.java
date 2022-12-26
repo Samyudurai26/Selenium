6f8d370bbfc99c8f15093d1cf4a57628c1d5c943
Samyudurai26 <selvasamyutha@gmail.com>
2022-12-26 19:25:38 +0530

    completed

--- src/org/sample/Samyutha.java
+++ src/org/sample/Samyutha.java
@@ -5,5 +5,11 @@ public class Samyutha {
 	public static void main(String[] args) {
 		 String s="java";
 		 System.out.println(s.charAt(2));
+		 
+		 int a=8;
+		 System.out.println(a);
+
+	    char gender='m';
+	    System.out.println(gender);
 	}
 }
